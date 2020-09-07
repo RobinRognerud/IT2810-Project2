fe98d340e959e79928dea80fd90046a0836a4923
ragnhild <ragnhola@stud.ntnu.no>
2020-09-07 11:04:45 +0200

    make footer

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
       <h1>Gruppe 51</h1>
+      <Footer />
     </div>
   );
 }
---
+++ my-app/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="footer">
+          <h1>Laget av oss :D </h1>
+        </div>
+      </footer>
+    );
+  }
+}
