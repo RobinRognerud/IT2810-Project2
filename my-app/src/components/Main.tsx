4dd824d0943332c947bb015d3925994bfaa98bb7
ragnhild <ragnhola@stud.ntnu.no>
2020-09-16 12:39:34 +0200

    add music

--- my-app/src/components/Main.tsx
+++ my-app/src/components/Main.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Music from "./Music";
 
 export default class Main extends Component {
   render() {
     return (
       <div className="home-container">
         <div className="picture-container">Bilder</div>
+        <div className="music1"> musikk</div>
         <div className="text-container">Våre installasjoner</div>
       </div>
     );
